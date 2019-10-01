4b355d699a53e1d803a4e014092548b6f4dbf6a4
Hideaki Murakami <thehiderofkeys@gmail.com>
2019-10-01 14:21:38 +1300

    Created Level sciprt for parallax. Closes #1

---
+++ Green Quest/Assets/Scripts/Level.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour{
+    [System.Serializable]
+    public class ParallaxElement{
+        public Transform background;
+        public float seperation;
+    }
+    public ParallaxElement[] parallaxElements;
+    private Vector3 cameraStartPosition;
+    // Start is called before the first frame update
+    void Start(){
+        cameraStartPosition = Camera.main.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 diff = Camera.main.transform.position - cameraStartPosition;
+        foreach(ParallaxElement parallaxElement in parallaxElements){
+            parallaxElement.background.localPosition = diff * parallaxElement.seperation;
+        }
+    }
+}
