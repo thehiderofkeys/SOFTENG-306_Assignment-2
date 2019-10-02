02c86098e3eba220b5abacff3b1c9c292867fa48
SamNeale <snea076@aucklanduni.ac.nz>
2019-10-03 12:59:23 +1300

    Implemented basic functionalility to load a new game

---
+++ Green Quest/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public Button newGameButton;
+    public Button loadGameButton;
+    public Button exitGameButton;
+
+    public string newGameSceneName;
+    public GameObject loadGameMenu;
+    
+
+    public void Awake()
+    {   
+
+        newGameButton.onClick.AddListener(NewGame);
+        loadGameButton.onClick.AddListener(OpenLoadGameMenu);
+        exitGameButton.onClick.AddListener(ExitGame);
+    }
+    public void NewGame()
+    {
+        SceneManager.LoadScene(newGameSceneName);
+    }
+
+  
+
+    public void OpenLoadGameMenu()
+    {
+        loadGameMenu.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit(); 
+    }
+}
