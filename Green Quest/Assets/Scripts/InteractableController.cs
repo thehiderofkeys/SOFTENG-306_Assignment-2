b9c21ce13b3a00c4be9169776e6ddb1e6a5a7a59
Joshua <joshua.sheng.fu@gmail.com>
2019-10-04 16:50:01 +1300

    Added and tested scoring system (pair-programmed with Ryan Lim @pabloSushibar)

--- Green Quest/Assets/Scripts/InteractableController.cs
+++ Green Quest/Assets/Scripts/InteractableController.cs
@@ -7,7 +7,7 @@ public class InteractableController : MonoBehaviour
 {
     public LayerMask playerLayer;
     private Collider2D collider2D;
-    public Sprite newSprite;
+    //public Sprite newSprite;
     private Sprite oldSprite;
     private Sprite temp;
     private SpriteRenderer renderer;
@@ -28,15 +28,6 @@ public class InteractableController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetAxis("Fire1") > 0)
-        //{
-        //    if (collider2D.IsTouchingLayers(playerLayer))
-        //    {
-
-        //        // Trigger light or something
-
-        //    }
-        //}
         if (collider2D.IsTouchingLayers(playerLayer))
         {
             if (Input.GetKeyDown(KeyCode.Space))
