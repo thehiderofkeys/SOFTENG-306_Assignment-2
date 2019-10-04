8084ebaf9733d6406997c316675da5fe5b99bcc0
Joshua <joshua.sheng.fu@gmail.com>
2019-10-04 14:19:10 +1300

    Switch is now functional (the corresponding light does not work yet)

--- Green Quest/Assets/Scripts/InteractableController.cs
+++ Green Quest/Assets/Scripts/InteractableController.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class InteractableController : MonoBehaviour
 {
+    public LayerMask playerLayer;
+    private Collider2D collider2D;
+    public Sprite newSprite;
+    private Sprite oldSprite;
+    private Sprite temp;
+    private SpriteRenderer renderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        collider2D = GetComponent<Collider2D>();
+        renderer = GetComponent<SpriteRenderer>();
     }
 
+
+   
+    
     // Update is called once per frame
     void Update()
     {
-        
+        //if (Input.GetAxis("Fire1") > 0)
+        //{
+        //    if (collider2D.IsTouchingLayers(playerLayer))
+        //    {
+
+        //        // Trigger light or something
+
+        //    }
+        //}
+        if (collider2D.IsTouchingLayers(playerLayer))
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                oldSprite = renderer.sprite;
+                print("space key was pressed");
+                renderer.sprite = newSprite;
+                temp = newSprite;
+                newSprite = oldSprite;
+                oldSprite = temp;
+            }
+        }
     }
+
 }
