5280e88c8580bf30386f89bbe498978ed4bc6a37
SamNeale <snea076@aucklanduni.ac.nz>
2019-10-05 16:07:37 +1300

    Pair programming with @isabellelucy - created functionality for respawn and edited exit screen functionality

---
+++ Green Quest/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class CollisionHandler : MonoBehaviour
+{
+    public UnityEvent colliderEvent;
+    public Collider2D collider;
+    public LayerMask playerLayer;
+
+
+    void Update()
+    {
+    }
+
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            colliderEvent.Invoke();
+        }
+    }
+}
