28b4cdba058e51f7788e09564236ed0d080d61f7
Hideaki Murakami <thehiderofkeys@gmail.com>
2019-10-06 11:56:42 +1300

    Created Tap. Fixes  #17

---
+++ Green Quest/Assets/Scripts/Tap.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tap : MonoBehaviour
+{
+    public void SetOn(bool isOn)
+    {
+        GetComponent<Animator>().SetBool("isOn", isOn);
+    }
+}
