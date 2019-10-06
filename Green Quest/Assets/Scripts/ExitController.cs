cdf30578c98dd866167078e62a55f8f9b4d6219b
isabellelucy <izzbee123@gmail.com>
2019-10-07 10:46:26 +1300

    Pair programming @SamNeale adding fail exit screen and altering main menu

---
+++ Green Quest/Assets/Scripts/ExitController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class ExitController : MonoBehaviour
+{
+
+    public Button nextLevel;
+    public Button retry;
+    public Button home;
+    public string nextScene;
+    public string retryScene;
+    public string homeScene;
+
+    public void ChangeNextScene()
+    {
+        SceneManager.LoadScene(nextScene); 
+    }
+
+    public void ChangeRetryScene()
+    {
+        SceneManager.LoadScene(retryScene);
+    }
+
+    public void ChangeHomeScene()
+    {
+        SceneManager.LoadScene(homeScene);
+    }
+
+}
