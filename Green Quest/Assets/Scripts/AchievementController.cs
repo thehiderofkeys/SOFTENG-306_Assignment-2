260a66c16c3ee662417bf790f53471f7dc90730a
isabellelucy <izzbee123@gmail.com>
2019-10-17 14:26:26 +1300

    Pair programming with @SamNeale . Created achievements system - number of stras shows up in exit screen. Fixes #63

--- Green Quest/Assets/Scripts/AchievementController.cs
+++ Green Quest/Assets/Scripts/AchievementController.cs
@@ -5,26 +5,27 @@ using UnityEngine.UI;
 
 public class AchievementController : MonoBehaviour
 {
-    public static List<int> starsForLevels = null;
+    public static List<int> starsForLevels =  new List<int>() { 0, 0, 0 };
+   // public Sprite achievedStar;
+   // public Sprite notAchievedStar;
+    public static AchievementController instance; 
 
     void Start()
     {
-        if (starsForLevels == null)
-        {
-            InitStarsArray();
-        }
-    }
-
-    void InitStarsArray()
-    {
-        starsForLevels = new List<int>() { 0, 0, 0 };
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+        
     }
 
     // Updates how many stars the user has acheived for the level
-    void UpdateStarsForLevel(int level, int starsAchieved)
+    public static void UpdateStarsForLevel(int level, int starsAchieved)
     {
         starsForLevels[level - 1] = starsAchieved;
     }
 
+    public List<int> GetStarsForLevels()
+    {
+        return starsForLevels; 
+    }
 
 }
