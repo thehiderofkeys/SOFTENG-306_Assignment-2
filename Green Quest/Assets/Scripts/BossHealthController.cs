fb64f5e3861b682fcb2ef4c9d3480eab13372924
SamNeale <snea076@aucklanduni.ac.nz>
2019-10-19 13:15:51 +1300

    Added boss lives and health controller script and icons to HUD for level 3

---
+++ Green Quest/Assets/Scripts/BossHealthController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHealthController : MonoBehaviour
+{
+
+
+    public List<Image> bossHealthImages;
+    public Sprite FilledSkull;
+    public Sprite EmptySkull;
+    private int numLivesReamining;
+    private static BossHealthController instance;
+
+
+    void Start()
+    {
+        instance = this;
+        instance.numLivesReamining = 6;
+    }
+
+
+    public void LoseLife()
+    {
+        instance.numLivesReamining--;
+        instance.bossHealthImages[instance.numLivesReamining].sprite = instance.EmptySkull;
+    }
+
+}
