36579e8344862f745c901413920024922ad282f3
Yinhu <41180818+YinhuC@users.noreply.github.com>
2019-10-21 17:48:10 +1300

    projectile script commented

--- Green Quest/Assets/Scripts/Projectile.cs
+++ Green Quest/Assets/Scripts/Projectile.cs
@@ -11,19 +11,23 @@ public class Projectile : MonoBehaviour
 
     void Start()
     {
+        // Set the velocity of the projectile, value changed in Unity
         GetComponent<Rigidbody2D>().velocity = initVelocity;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        // If the projectile hits the layer or user then invoke method to 
         if (TargetLayer == (TargetLayer | 1 << collision.gameObject.layer))
         {
             OnCollide.Invoke();
         }
+        // Destroy the projectile
         Destroy(gameObject);
     }
     public void StunBoss()
     {
+        // Set the boss to invulnerable for 5 seconds
         BossController.instance.StartVolnurable(5);
     }
 }
